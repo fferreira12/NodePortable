3d8a5e948ed544d7d4b43b0af11d94cd5e28b379
Fernando <fferreira12@gmail.com>
2017-08-02 13:59:58 -0300

    initial commit from csb pc

---
+++ NodeInstallerTests/MainTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NodePortable;
+using System.IO;
+
+namespace NodeInstallerTests
+{
+    [TestClass]
+    public class MainTests
+    {
+
+        static NodeInstaller installer;
+        static string installPath = "C:/tests";
+
+        [ClassInitialize()]
+        public static void Initiate(TestContext t)
+        {
+            installer = new NodeInstaller(installPath);
+        }
+
+        [TestMethod]
+        public void TestCopyNodeBinaryToFolder()
+        {
+            installer.CopyNodeBinaryToFolder();
+            Assert.IsTrue(File.Exists(installPath + "/node.zip"));
+        }
+
+        [TestMethod]
+        public void TestExtractBinary()
+        {
+            installer.ExtractBinary();
+        }
+    }
+}
---
+++ NodePortable/NodeInstaller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodePortable
+{
+    public class NodeInstaller
+    {
+
+        string InstallationPath { get; set; }
+        byte[] Node = Properties.Resources.node;
+        string fullName;
+
+
+        public NodeInstaller(string installationPath)
+        {
+
+            InstallationPath = installationPath;
+            fullName = InstallationPath + "/node.zip";
+
+        }
+
+        public void CopyNodeBinaryToFolder()
+        {
+            if (!File.Exists(fullName))
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(fullName, FileMode.Create)))
+                {
+                    writer.Write(Node);
+                } 
+            }
+        }
+
+        public void ExtractBinary()
+        {
+            ZipFile.ExtractToDirectory(fullName, InstallationPath);
+        }
+
+    }
+}
